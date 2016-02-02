a2193dba2482a66bbf3950d61ed4dd71f77c7773
Xiangyu Li <haoyushijie@cometnet-10-21-44-153.utdallas.edu>
2016-02-02 09:58:41 -0600

    Initial Commit

---
+++ Orange/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Orange
+//
+//  Created by Xiangyu Li on 2/2/16.
+//  Copyright (c) 2016 Xiangyu Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
